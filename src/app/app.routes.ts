cbbb4f4bd29d9f4527107d9f04e0955033998644
limhokyun <lim@nsmarts.co.kr>
2024-03-25 14:53:20 +0900

    [fix] 설문조사 화면으로 이동 navigate 추가

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,7 @@ export const routes: Routes = [
         component: HomeComponent
     },
     {
-        path: 'survey',
+        path: 'survey/:_id',
         title: 'servey',
         component: ServeyComponent
     },
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CardComponent } from '../../components/public/card/card.component';
 import { SurveyService } from '../../services/survey/survey.service';
 import { ThisReceiver } from '@angular/compiler';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -14,11 +15,16 @@ export class HomeComponent {
 
   surveys: Array<any> = [];
 
-  constructor(private surveyService: SurveyService) { }
+  constructor(private surveyService: SurveyService, private router: Router) { }
 
   ngOnInit() {
     this.surveyService.getSurveyList().subscribe((res: any) => {
       this.surveys = res
     })
+  }
+
+  // 설문조사로 이동
+  goToSurvey(_id: string) {
+    this.router.navigate([`survey/${_id}`])
   }
 }
